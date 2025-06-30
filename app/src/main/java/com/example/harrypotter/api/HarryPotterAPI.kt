24e7d2aca84af42dd6825549b7cf07f3eb2a7813
Viviane Mori <vivianeendomori@gmail.com>
2025-06-30 11:46:05 -0300

    fix: ajuste harry potter api kt

--- app/src/main/java/com/example/harrypotter/api/HarryPotterAPI.kt
+++ app/src/main/java/com/example/harrypotter/api/HarryPotterAPI.kt
@@ -1,5 +1,7 @@
 package com.example.harrypotter.api
 
+import android.R
+import com.google.gson.annotations.SerializedName
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,18 +11,21 @@ interface HarryPotterAPI {
     @GET("character/{personagemId}")
     suspend fun getPersonagemInfo(
         @Path("personagemId") personagemId: String
-    ): Response<Map<String, PersonagemInfo>>
+    ): Response<PersonagemInfo>
 
     @GET("characters/staff")
-    suspend fun getAllProfessores(): Response<Map<List<String>, ProfessorInfo>>
+    suspend fun getAllProfessores(): Response<List<ProfessorInfo>>
+
+    @GET("characters/students")
+    suspend fun getAllStudents(): Response<List<EstudanteInfo>>
 
     @GET("characters/house/{houseName}")
     suspend fun getCharactersByHouse(
         @Path("houseName") houseName: String
-    ): Response<Map<List<String>, PersonagemInfo>>
+    ): Response<List<PersonagemInfo>>
 
     @GET("spells")
-    suspend fun getAllSpells(): Response<Map<List<String>, SpellInfo>>
+    suspend fun getAllSpells(): Response<List<SpellInfo>>
 }
 
 data class PersonagemInfo(
@@ -31,16 +36,62 @@ data class PersonagemInfo(
     val image: String
 )
 
+data class EstudanteInfo(
+    val id: String,
+    val name: String,
+    @SerializedName("alternate_names")
+    val alternateNames: List<String> = emptyList(),
+    val species: String,
+    val gender: String,
+    val house: String,
+    val dateOfBirth: String,
+    val yearOfBirth: String,
+    val wizard: Boolean,
+    val ancestry: String,
+    val eyeColour: String,
+    val hairColour: String,
+    val wand: WandInfo,
+    val patronus: String,
+    val hogwartsStudent: Boolean,
+    val hogwartsStaff: Boolean,
+    val actor: String,
+    @SerializedName("alternate_actors")
+    val alternateActors: List<String> = emptyList(),
+)
+
 data class ProfessorInfo(
     val id: String,
     val name: String,
+    @SerializedName("alternate_names")
+    val alternateNames: List<String> = emptyList(),
     val species: String,
+    val gender: String,
     val house: String,
-    val image: String,
-    val alternate_names: List<String> = emptyList()
+    val dateOfBirth: String,
+    val yearOfBirth: String,
+    val wizard: Boolean,
+    val ancestry: String,
+    val eyeColour: String,
+    val hairColour: String,
+    val wand: WandInfo,
+    val patronus: String,
+    val hogwartsStudent: Boolean,
+    val hogwartsStaff: Boolean,
+    val actor: String,
+    @SerializedName("alternate_actors")
+    val alternateActors: List<String> = emptyList(),
+    val alive: R.bool,
+    val image: String
+)
+
+data class WandInfo(
+    val wood: String,
+    val core: String,
+    val length: Int
 )
 
 data class SpellInfo(
+    val id: String,
     val name: String,
     val description: String
 )
