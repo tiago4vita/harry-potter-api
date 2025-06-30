1777f9fddf02be4fa34a625477bcc32df842adac
andrejpg <andremoraisbrito556@gmail.com>
2025-06-30 03:11:55 -0300

    feat(front-end): configura main activity, adiciona icons, adiciona finishActivity do botão de sair, permite acesso a internet

--- app/src/main/java/com/example/harrypotter/MainActivity.kt
+++ app/src/main/java/com/example/harrypotter/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.harrypotter
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btnListarPersonagem = findViewById<Button>(R.id.btnListarPersonagem)
+        btnListarPersonagem.setOnClickListener {
+                val intent = Intent(this, PersonagemActivity::class.java)
+                startActivity(intent)
+        }
+
+        val btnListarProfessor = findViewById<Button>(R.id.btnListarProfessor)
+        btnListarProfessor.setOnClickListener {
+                val intent = Intent(this, ProfessorActivity::class.java)
+                startActivity(intent)
+        }
+
+        val btnListarEstudantes = findViewById<Button>(R.id.btnListarEstudantes)
+        btnListarEstudantes.setOnClickListener {
+                val intent = Intent(this, EstudantesActivity::class.java)
+                startActivity(intent)
+        }
+
+        val btnVerFeiticos = findViewById<Button>(R.id.btnVerFeiticos)
+        btnVerFeiticos.setOnClickListener {
+                val intent = Intent(this, FeiticosActivity::class.java)
+                startActivity(intent)
+        }
+
+        val btnSair = findViewById<Button>(R.id.btnSair)
+        btnSair.setOnClickListener {
+                finishAffinity()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
